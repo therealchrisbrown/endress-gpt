1b820898dd4279848d27bc0f099c3de9e7f1bbcf
therealchrisbrown <ch.braun1@gmx.de>
2023-06-26 13:41:32 +0200

    own data

--- endress-gpt.py
+++ endress-gpt.py
@@ -1,9 +1,19 @@
 import os
 import sys
 
-from decouple import config
+from langchain.document_loaders import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 
-OPENAI_API_KEY = config('OPENAI_API_KEY')
+import constants
+
+os.environ["OPENAI_API_KEY"] = constants.OPENAI_API_KEY
 
 query = sys.argv[1]
 print(query)
+
+loader = TextLoader('data.txt')
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+print(index.query(query))
